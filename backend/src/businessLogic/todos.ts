4c2d0fc0553aa73807d363d88544e974f4f038cc
nicholasleong <leongheinwai97@gmail.com>
2022-01-20 01:16:22 +1100

    frontend, auth, getTodo and createTodo working

---
+++ backend/src/businessLogic/todos.ts
@@ -0,0 +1,60 @@
+import { TodosAccess } from './todosAcess'
+// import { AttachmentUtils } from './attachmentUtils';
+import { TodoItem } from '../models/TodoItem'
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import { createLogger } from '../utils/logger'
+import * as uuid from 'uuid'
+// import * as createError from 'http-errors'
+import { TodoUpdate } from '../models/TodoUpdate';
+
+const logger = createLogger('TodoBusinessLogic')
+const todosAccess = new TodosAccess()
+
+// Get todo func
+export async function getTodos(userId: string): Promise<TodoItem[]> {
+    logger.info(`Get todo for userId ${ userId }`)
+    return await todosAccess.getAllTodosbyuserId(userId)
+}
+
+// Create todo func
+export async function createTodo(
+    createTodoRequest: CreateTodoRequest, userId: string
+): Promise<TodoItem> {
+
+    const todoId = uuid.v4()
+
+    const todo: TodoItem = {
+        userId: userId,
+        todoId: todoId,
+        createdAt: new Date().toISOString(),
+        name: createTodoRequest.name,
+        dueDate: createTodoRequest.dueDate,
+        done: false
+    }
+
+    logger.info(`Create todo for userId ${ userId }`)
+    return await todosAccess.createTodo(todo)
+}
+
+// Update todo func
+export async function updateTodo(
+    userId: string, todoId: string, updateTodoRequest: UpdateTodoRequest
+): Promise<TodoItem> {
+    const updateTodo: TodoUpdate = {
+        name: updateTodoRequest.name,
+        dueDate: updateTodoRequest.dueDate,
+        done: updateTodoRequest.done
+    }
+    
+    logger.info(`UserId ${ userId } update todo with todoId ${ todoId }`)
+    return await todosAccess.updateTodo(userId, todoId, updateTodo)
+}
+
+// Delete todo func
+export async function deleteTodo(userId: string, todoId: string): Promise<String> {
+    logger.info(`UserId ${ userId } delete todo with todoId ${ todoId }`)
+    return await todosAccess.deleteTodos(userId, todoId)
+}
+
+// TODO: S3 stuff
---
+++ backend/src/businessLogic/todosAcess.ts
@@ -0,0 +1,91 @@
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import { createLogger } from '../utils/logger'
+import { TodoItem } from '../models/TodoItem'
+import { TodoUpdate } from '../models/TodoUpdate';
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const logger = createLogger('TodosAccess')
+
+// Implement the dataLayer logic
+export class TodosAccess {
+
+    constructor(
+        private readonly docClient: DocumentClient = createDynamoDBClient(),
+        private readonly todosTable = process.env.TODOS_TABLE){
+    }
+
+    async getAllTodosbyuserId(userId: string): Promise<TodoItem[]> {
+        const result = await this.docClient.query({
+            TableName: this.todosTable,
+            KeyConditionExpression: 'userId = :userId',
+            ExpressionAttributeValues: {
+                ':userId': userId
+            }
+        }).promise()
+        
+        logger.info(`Todo's for userId ${ userId } retrieve successfully`)
+        const items = result.Items
+        return items as TodoItem[]
+    }
+
+    async createTodo(todo: TodoItem): Promise<TodoItem> {
+        await this.docClient.put({
+            TableName: this.todosTable,
+            Item: todo
+        }).promise()
+        
+        logger.info('Todo created successfully')
+        return todo
+    }
+
+    async updateTodo(userId: string, todoId: string, update_todo: TodoUpdate): Promise<TodoItem> {
+        await this.docClient.update({
+            TableName: this.todosTable,
+            Key: {
+                'todoId': todoId,
+                'userId': userId
+            },
+            UpdateExpression: "set name = :n, dueDate=:d, done=:c",
+            ExpressionAttributeValues:{
+                ":n": update_todo.name,
+                ":d": update_todo.dueDate,
+                ":c": update_todo.done
+            }
+        }).promise()
+        logger.info('Todo updated successfully')
+
+        const result = await this.docClient.query({
+            TableName : this.todosTable,
+            KeyConditionExpression: 'todoID = :todoID',
+            ExpressionAttributeValues: {
+                ':todoID': todoId
+            }
+        }).promise()
+        
+
+        const todo = result.Items[0]
+        return todo as TodoItem
+    }
+
+    async deleteTodos(userId: string, todoId: string): Promise<String> {
+        await this.docClient.delete({
+            TableName: this.todosTable,
+            Key: {
+                'todoId': todoId,
+                'userId': userId
+            }
+        }).promise()
+        
+        logger.info('detete todo successfully')
+        return ''
+    }
+
+    // TODO: S3 stuff
+}
+
+function createDynamoDBClient() {
+    return new XAWS.DynamoDB.DocumentClient()
+}
